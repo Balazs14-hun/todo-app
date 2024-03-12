08bd2ee8217f0f7d61b480beafb8d592955b46fe
Dobai Balazs <balazsdobai1@gmail.com>
2024-03-12 18:00:34 +0200

    Fixed requested changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import HeaderComponent from './components/HeaderComponent.vue'
-</script>
-
 <template>
   <div class="flex flex-col justify-center content-center mx-auto max-w-screen-sm">
     <HeaderComponent />
   </div>
 </template>
+
+<script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue'
+</script>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,14 +1,17 @@
-<script setup lang="ts">
-const createTodo = () => {
-  console.log('Create todo...')
-}
-</script>
-
 <template>
   <div class="flex items-center justify-between">
     <h1 class="font-semibold text-7xl">To do list</h1>
-    <button @click="createTodo">
-      <img src="../assets/svg/add-button.svg" />
-    </button>
+    <BaseButton>
+      <PlusCircleIcon class="w-14 h-14 text-green-400 cursor-pointer" @click="createTodo" />
+    </BaseButton>
   </div>
 </template>
+
+<script setup lang="ts">
+import BaseButton from '@/components/BaseButton.vue'
+import { PlusCircleIcon } from '@heroicons/vue/24/solid'
+
+function createTodo() {
+  console.log('Create todo...')
+}
+</script>
