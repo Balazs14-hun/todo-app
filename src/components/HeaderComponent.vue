f36cede34f6bf44b15e4c6724091684395afba49
Dobai Balazs <balazsdobai1@gmail.com>
2024-03-12 16:18:28 +0200

    ITL-10 Create the event that triggers the todo creation functionality

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const createTodo = () => {
+  console.log('Create todo...')
+}
+</script>
 
 <template>
   <div class="flex items-center justify-between">
     <h1 class="font-semibold text-7xl">To do list</h1>
-    <button>
+    <button @click="createTodo">
       <img src="../assets/svg/add-button.svg" />
     </button>
   </div>
